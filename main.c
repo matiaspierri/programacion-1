d91089c798c633710e48316a130781e50e8db439
Fran2109 <82397975+Fran2109@users.noreply.github.com>
2021-11-28 11:36:43 -0300

    Clientes terminado

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 
 #define VIP 800000
-#define historial_texto "historial_de_compras.txt"
 #define productos_binario "productos.bin"
 #define clientes_binario "clientes.bin"
 
@@ -75,7 +74,23 @@ void menu_admin_productos(Lista_enlazada_producto * lista_datos_productos);
 
 //Funciones pantalla clientes
 void cargar_clientes(Lista_enlazada_cliente * lista);
+void ingresar_cliente(Clientes * cliente);
+void listar_clientes(Lista_enlazada_cliente *lista);
+void imprimir_cliente( Clientes cliente, int i);
+void modificar_cliente(Lista_enlazada_cliente * lista);
+int buscar_cliente(Lista_enlazada_cliente * lista, char * mensaje);
+void eliminar_cliente(Lista_enlazada_cliente * lista);
+void eliminar_cliente_cabeza(Lista_enlazada_cliente * lista);
+void eliminar_cliente_cola(Lista_enlazada_cliente * lista);
+void eliminar_cliente_medio(Lista_enlazada_cliente * lista, int id);
+void listar_clientes_vip(Lista_enlazada_cliente *lista);
+void listar_clientes_sin_compra(Lista_enlazada_cliente * lista);
 
+//Funciones lista clientes
+void insertar_nodo_cliente(Lista_enlazada_cliente *lista, Clientes *datos);
+struct Nodo_cliente *crear_nodo_cliente(Clientes *datos);
+void lista_clientes_a_archivo(Lista_enlazada_cliente *lista);
+void archivo_a_lista_clientes(Lista_enlazada_cliente *lista);
 
 //Funciones pantalla productos
 void cargar_productos(Lista_enlazada_producto * lista);
@@ -110,6 +125,7 @@ int main()
     Lista_enlazada_cliente *Lista_cliente = (Lista_enlazada_cliente *)calloc(1, sizeof(Lista_enlazada_cliente));
     Lista_enlazada_producto *Lista_producto = (Lista_enlazada_producto *)calloc(1, sizeof(Lista_enlazada_producto));
     archivo_a_lista_productos(Lista_producto);
+    archivo_a_lista_clientes(Lista_cliente);
     do
     {
         borrar_pantalla();
@@ -176,22 +192,19 @@ void menu_admin_clientes(Lista_enlazada_cliente * lista_datos_clientes)
             cargar_clientes(lista_datos_clientes);
             break;
         case 2:
-            //Opcion 2: Modificar Cliente.
+            modificar_cliente(lista_datos_clientes);
             break;
         case 3:
-            //Opcion 3: Borrar Cliente.
+            eliminar_cliente(lista_datos_clientes);
             break;
         case 4:
-            //Opcion 4: Listar Clientes.
+            listar_clientes(lista_datos_clientes);
             break;
         case 5:
-            //Opcion 5: Listar Clientes VIP.
+            listar_clientes_vip(lista_datos_clientes);
             break;
         case 6:
-            //Opcion 6: Listar Clientes sin compras.
-            break;
-        case 7:
-            //Opcion 7: Volver al Menu admin.
+            listar_clientes_sin_compra(lista_datos_clientes);
             break;
         default:
             invalida();
@@ -307,22 +320,330 @@ int imprimir_menu_admin_facturacion()
 void cargar_clientes(Lista_enlazada_cliente * lista)
 {
     char opcion = 'S';
-    Productos * cliente = malloc(sizeof(Clientes));
+    Clientes * cliente = malloc(sizeof(Clientes));
 
     while (toupper(opcion) == 'S')
     {
         borrar_pantalla();
-        //ingresar_producto(producto);
-        //producto->vendidos=0;
-        //insertar_nodo_producto(lista, producto);
-        //printf("\n\nQuiere ingresar otro producto (S / N): ");
-        //fflush(stdin);
-        //scanf(" %c", &opcion);
+        ingresar_cliente(cliente);
+        cliente->cant_dias_sin_Comprar = -1;
+        cliente->cantidad_facturacion = 0;
+        cliente->cliente_vip = 0;
+        insertar_nodo_cliente(lista, cliente);
+        printf("\nQuiere ingresar otro Cliente (S / N): ");
+        fflush(stdin);
+        scanf(" %c", &opcion);
     }
-    //lista_productos_a_archivo(lista);
+    lista_clientes_a_archivo(lista);
 
 }
 
+void insertar_nodo_cliente(Lista_enlazada_cliente *lista, Clientes *datos)
+{
+    struct Nodo_cliente *nuevo_nodo = crear_nodo_cliente(datos);
+    if (lista->tam == 0) // Primera Insercion
+    {
+        lista->cabeza = nuevo_nodo;
+        lista->cola = nuevo_nodo;
+    }
+    else
+    {
+        lista->cola->siguiente = nuevo_nodo;
+        lista->cola = nuevo_nodo;
+    }
+    lista->tam++;
+    return;
+}
+
+struct Nodo_cliente *crear_nodo_cliente(Clientes *datos)
+{
+    struct Nodo_cliente *nuevo_nodo = (struct Nodo_cliente *)calloc(1, sizeof(struct Nodo_cliente));
+
+    if (nuevo_nodo == NULL)
+    {
+        printf("Insuficiente Memoria");
+        exit(-1);
+    }
+
+    nuevo_nodo->datos = *datos;
+    nuevo_nodo->siguiente = NULL;
+
+    return nuevo_nodo;
+}
+
+void ingresar_cliente(Clientes * cliente)
+{
+    printf("\nCargar datos del cliente:\n");
+    printf("Razon social: ");
+    fflush(stdin);
+    gets(cliente->razon_social);
+    printf("Rubro de la empresa: ");
+    fflush(stdin);
+    gets(cliente->rubro);
+    printf("Direccion\n");
+    printf("Calle: ");
+    fflush(stdin);
+    gets(cliente->direccion.calle);
+    printf("Altura: ");
+    scanf("%d", &cliente->direccion.altura);
+    printf("Piso: ");
+    fflush(stdin);
+    gets(cliente->direccion.piso);
+}
+
+void listar_clientes(Lista_enlazada_cliente *lista)
+{
+    borrar_pantalla();
+    if (lista->tam == 0)
+    {
+        printf("\nNo hay Clientes\n");
+        frenar();
+        return;
+    }
+    struct Nodo_cliente *nodo_actual = lista->cabeza;
+
+    for (int i = 0; i < lista->tam; i++)
+    {
+        imprimir_cliente(nodo_actual->datos, i);
+        //frenar();
+        nodo_actual = nodo_actual->siguiente;
+    }
+    printf("\nFin de los clientes\n");
+    frenar();
+    return;
+}
+
+void imprimir_cliente( Clientes cliente, int i)
+{
+    printf("\nCliente N%d\n", i+1);
+    printf("Razon Social: %s\n", cliente.razon_social);
+    printf("Rubro: %s\n", cliente.rubro);
+    printf("Cantidad de dias sin comprar: ");
+    if(cliente.cant_dias_sin_Comprar==-1){ printf("Nunca compro\n"); } else { printf("%d\n", cliente.cant_dias_sin_Comprar); }
+    printf("Cantidad Facturado: $%d\n", cliente.cantidad_facturacion);
+    printf("Es VIP: ");
+    if(cliente.cliente_vip==1) { printf("SI\n"); } else { printf("NO\n"); }
+    printf("Direccion\n");
+    printf("Calle: %s\n", cliente.direccion.calle);
+    printf("Altura: %d\n", cliente.direccion.altura);
+    printf("Piso: %s\n", cliente.direccion.piso);
+}
+
+void modificar_cliente(Lista_enlazada_cliente * lista)
+{
+    if (lista->tam == 0)
+    {
+        printf("\nNo hay Clientes\n");
+        frenar();
+        return;
+    }
+    else
+    {
+        int id;
+        printf("Lista de Clientes: ");
+        listar_clientes(lista);
+        id = buscar_cliente(lista, "Ingrese el ID del cliente a editar: ");
+        struct Nodo_cliente *nodo_actual = lista->cabeza;
+        for(int i=0; i<id ; i++)
+        {
+            if(i+1==id)
+            {
+                imprimir_cliente(nodo_actual->datos, i);
+                printf("\nIngrese los nuevos datos\n");
+                ingresar_cliente(&nodo_actual->datos);
+                lista_clientes_a_archivo(lista);
+                printf("\Cliente actualizado correctamente\n");
+                break;
+            }
+            else
+            {
+                nodo_actual = nodo_actual->siguiente;
+            }
+        }
+        frenar();
+    }
+}
+
+int buscar_cliente(Lista_enlazada_cliente * lista, char * mensaje)
+{
+    int id=lista->tam+1;
+    while(id>(lista->tam))
+    {
+        printf("\n%s", mensaje);
+        scanf("%d", &id);
+        if(id>(lista->tam))
+        {
+            invalida();
+        }
+    }
+    return id;
+}
+
+void eliminar_cliente(Lista_enlazada_cliente * lista)
+{
+    if (lista->tam == 0)
+    {
+        printf("\nNo hay Clientes\n");
+        frenar();
+        return;
+    }
+    else
+    {
+        int id;
+        printf("Lista de Cliente: ");
+        listar_clientes(lista);
+        id = buscar_cliente(lista, "Ingrese el ID del Cliente a eliminar: ");
+        if(id==1)
+        {
+            eliminar_cliente_cabeza(lista);
+        }
+        else if (id==lista->tam)
+        {
+            eliminar_cliente_cola(lista);
+        }
+        else
+        {
+            eliminar_cliente_medio(lista, id);
+        }
+
+        lista_clientes_a_archivo(lista);
+        printf("\nCliente borrado correctamente");
+        frenar();
+    }
+}
+
+void eliminar_cliente_cabeza(Lista_enlazada_cliente * lista)
+{
+    struct Nodo_cliente *segundo_nodo = lista->cabeza->siguiente;
+    lista->cabeza = segundo_nodo;
+    lista->tam--;
+    return;
+}
+
+void eliminar_cliente_cola(Lista_enlazada_cliente * lista)
+{
+    struct Nodo_cliente *nodo_actual = lista->cabeza;
+    for (int i = 1; i < lista->tam; i++)
+    {
+        nodo_actual = nodo_actual->siguiente;
+    }
+    nodo_actual->siguiente = NULL;
+    lista->cola = nodo_actual;
+    lista->tam--;
+    return;
+}
+
+void eliminar_cliente_medio(Lista_enlazada_cliente * lista, int id)
+{
+    struct Nodo_cliente *nodo_actual = lista->cabeza;
+    struct Nodo_cliente *nodo_anterior = NULL;
+    for (int i = 1; i < id; i++)
+    {
+        nodo_anterior = nodo_actual;
+        nodo_actual = nodo_actual->siguiente;
+    }
+    nodo_anterior->siguiente = nodo_actual->siguiente;
+    lista->tam--;
+    return;
+}
+
+void listar_clientes_vip(Lista_enlazada_cliente *lista)
+{
+    int c=0;
+    borrar_pantalla();
+    if (lista->tam == 0)
+    {
+        printf("\nNo hay Clientes\n");
+        frenar();
+        return;
+    }
+    struct Nodo_cliente *nodo_actual = lista->cabeza;
+    printf("\nLista clientes VIP\n");
+    for (int i = 0; i < lista->tam; i++)
+    {
+        if(nodo_actual->datos.cliente_vip==1)
+        {
+            c=1;
+            imprimir_cliente(nodo_actual->datos, i);
+        }
+        nodo_actual = nodo_actual->siguiente;
+    }
+    if(c==1)
+    {
+        printf("\nFin de los clientes\n");
+    }
+    else
+    {
+        printf("\nNo hay clientes VIP\n");
+    }
+    frenar();
+    return;
+}
+
+void listar_clientes_sin_compra(Lista_enlazada_cliente *lista)
+{
+    int c=0;
+    borrar_pantalla();
+    if (lista->tam == 0)
+    {
+        printf("\nNo hay Clientes\n");
+        frenar();
+        return;
+    }
+    struct Nodo_cliente *nodo_actual = lista->cabeza;
+    printf("\nLista clientes Sin Compras\n");
+    for (int i = 0; i < lista->tam; i++)
+    {
+        if(nodo_actual->datos.cant_dias_sin_Comprar==-1)
+        {
+            c=1;
+            imprimir_cliente(nodo_actual->datos, i);
+        }
+        nodo_actual = nodo_actual->siguiente;
+    }
+    if(c==1)
+    {
+        printf("\nFin de los clientes\n");
+    }
+    else
+    {
+        printf("\nNo hay clientes sin comprar\n");
+    }
+    frenar();
+    return;
+}
+
+
+
+void lista_clientes_a_archivo(Lista_enlazada_cliente *lista)
+{
+    vaciar_archivo(clientes_binario);
+    FILE * archivo = abrir_archivo(clientes_binario, "ab");
+    struct Nodo_cliente *nodo_actual = lista->cabeza;
+    for (int i = 0; i < lista->tam; i++)
+    {
+        fwrite(&nodo_actual->datos, sizeof(Clientes), 1, archivo);
+        nodo_actual = nodo_actual->siguiente;
+    }
+    fclose(archivo);
+}
+
+void archivo_a_lista_clientes(Lista_enlazada_cliente *lista)
+{
+    if(archivo_existe(clientes_binario) == 1)
+    {
+        FILE *archivo=abrir_archivo(clientes_binario, "rb");
+        Clientes *cliente = malloc(sizeof(Clientes));
+        fseek(archivo, 0, SEEK_SET);
+        fread(cliente, sizeof(Clientes), 1, archivo);
+        while(!feof(archivo))
+        {
+            insertar_nodo_cliente(lista, cliente);
+            fread(cliente, sizeof(Clientes), 1, archivo);
+        }
+        free(cliente);
+    }
+}
 
 void cargar_productos(Lista_enlazada_producto * lista)
 {
@@ -339,6 +660,7 @@ void cargar_productos(Lista_enlazada_producto * lista)
         fflush(stdin);
         scanf(" %c", &opcion);
     }
+    free(producto);
     lista_productos_a_archivo(lista);
 }
 
@@ -414,6 +736,7 @@ void modificar_producto(Lista_enlazada_producto * lista)
                 ingresar_producto(&nodo_actual->datos);
                 lista_productos_a_archivo(lista);
                 printf("\nProducto actualizado correctamente\n");
+                break;
             }
             else
             {
@@ -422,7 +745,6 @@ void modificar_producto(Lista_enlazada_producto * lista)
         }
         frenar();
     }
-
 }
 
 void eliminar_producto(Lista_enlazada_producto * lista)
@@ -460,11 +782,10 @@ void eliminar_producto(Lista_enlazada_producto * lista)
 
 void eliminar_producto_cabeza(Lista_enlazada_producto * lista)
 {
-    struct Node *segundo_nodo = lista->cabeza->siguiente;
+    struct Nodo_producto *segundo_nodo = lista->cabeza->siguiente;
 
     lista->cabeza = segundo_nodo;
     lista->tam--;
-    //free(lista->cabeza->siguiente);
     return;
 }
 
@@ -584,7 +905,6 @@ void lista_productos_a_archivo(Lista_enlazada_producto *lista)
     vaciar_archivo(productos_binario);
     FILE * archivo = abrir_archivo(productos_binario, "ab");
     struct Nodo_producto *nodo_actual = lista->cabeza;
-    Productos prod;
     for (int i = 0; i < lista->tam; i++)
     {
         fwrite(&nodo_actual->datos, sizeof(Productos), 1, archivo);
